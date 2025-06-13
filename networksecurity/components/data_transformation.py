84de27411ba1df33713ac7bec18d87576c2af0ca
ridabayi <bayi.rida@gmail.com>
2025-06-13 05:59:17 +0200

    add firt step of data transformation

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,12 @@
+import os, sys
+import numpy as np
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+from networksecurity.constant.training_pipeline import TARGET_COLUMN, DATA_TRANSFORMATION_IMPUTER_PARAMS
+from networksecurity.entity.artifact_entity import DataTransformationArtifact, DataValidationArtifact
+
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_object, save_numpy_array_data
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -35,3 +35,28 @@ def write_yaml_file(file_path: str, content: object, replace:bool = False) -> No
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Saves a NumPy array to a file using pickle.
+    
+    :param file_path: Path to the file where the array will be saved.
+    :param array: NumPy array to save.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object):
+    try:
+        logging.info("Entred the save_object method of mainutils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of mainutils class")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
