cac060ffbb4ae963953a43071c33620266a93e46
ridabayi <bayi.rida@gmail.com>
2025-06-16 14:49:43 +0200

    create trained model config

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,6 +1,11 @@
 import os, sys
+from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
+from networksecurity.entity.config_entity import ModelTrainingConfig
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.main_utils.utils import save_object, load_object
+from networksecurity.utils.main_utils.utils import load_numpy_array_data, evaluate_models
+from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
+from networksecurity.exception.exception import NetworkSecurityException
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import r2_score
@@ -19,15 +24,41 @@ class ModelTrainer:
             raise NetworkSecurityException(e, sys)
 
     def train_model(self, x_train, y_train, x_test_ y_test):
-        try:
-            pass
-        except Exception as e:  
-            raise NetworkSecurityException(e, sys)
+        models = {"random Forest": RandomForestClassifier(verbose=1),
+                  "Decision Tree": DecisionTreeClassifier(),
+                  "Gradient Boosting": GradientBoostingClassifier(verbose=1),
+                  "Logistic Regression": LogisticRegression(verbose=1),
+                  "AdaBoost": AdaBoostClassifier()}
+        
+        params={
+            "Decision Tree": {
+                'criterion':['gini', 'entropy', 'log_loss'],
+                # 'splitter':['best','random'],
+                # 'max_features':['sqrt','log2'],
+            },
+            "Random Forest":{
+                # 'criterion':['gini', 'entropy', 'log_loss'],
+                
+                # 'max_features':['sqrt','log2',None],
+                'n_estimators': [8,16,32,128,256]
+            },
+            "Gradient Boosting":{
+                # 'loss':['log_loss', 'exponential'],
+                'learning_rate':[.1,.01,.05,.001],
+                'subsample':[0.6,0.7,0.75,0.85,0.9],
+                # 'criterion':['squared_error', 'friedman_mse'],
+                # 'max_features':['auto','sqrt','log2'],
+                'n_estimators': [8,16,32,64,128,256]
+            },
+            "Logistic Regression":{},
+            "AdaBoost":{
+                'learning_rate':[.1,.01,.001],
+                'n_estimators': [8,16,32,64,128,256]
+            }
+            
+        }
 
-
-
-
-
+        model_report:dict = evaluate_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, models=models, params=params)
 
 
 
